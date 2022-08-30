14f692d26ed08d36aeeff42a33f318382fff742a
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-08-29 22:44:34 -0500

    Add Initial Player Movement

--- Kart Racer/Assets/Scripts/Actor/Player/PlayerInputController.cs
+++ Kart Racer/Assets/Scripts/Actor/Player/PlayerInputController.cs
@@ -1,8 +1,24 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerInputController : MonoBehaviour
 {
-   public DefaultInputActions.PlayerActions _input;
-    
+    public bool IsAccelerating { get; set; }
+    public bool IsBraking { get; set; }
+    public bool IsDrifting { get; set; }
+    public bool IsUsingItem { get; set; }
+    public Vector2 Steering { get; set; }
+
+    public void OnAccelerate(InputValue val) => IsAccelerating = val.isPressed;
+
+    public void OnBrakeReverse(InputValue val) => IsBraking = val.isPressed;
+
+    public void OnDrift(InputValue val) => IsDrifting = val.isPressed;
+
+    public void OnItem(InputValue val) => IsUsingItem = val.isPressed;
+
+    public void OnSteer(InputValue val) => Steering = val.Get<Vector2>();
+
 }
