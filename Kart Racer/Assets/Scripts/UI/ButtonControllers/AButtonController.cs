9ce32a1ccc8d0f631f5db8d6c034d2c71bc9c095
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-09-01 17:23:12 -0500

    Add GamePad Menu Navigation

--- Kart Racer/Assets/Scripts/UI/ButtonControllers/AButtonController.cs
+++ Kart Racer/Assets/Scripts/UI/ButtonControllers/AButtonController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.ButtonControllers
 {
     public abstract class AButtonController : MonoBehaviour
     {
         protected CanvasController _canvasController;
+        protected Button _button;
 
         void Awake()
         {
             _canvasController = GetComponentInParent<CanvasController>();
+            _button = GetComponent<Button>();
         }
 
+        public void Select() => _button.Select();
+
         public abstract void OnClick();
+
     }
 }
--- Kart Racer/Assets/Scripts/UI/UIController.cs
+++ Kart Racer/Assets/Scripts/UI/UIController.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using UI.ButtonControllers;
 using Util.Helpers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -7,14 +11,27 @@ namespace UI
     {
         public Util.Enums.UI UIType;
 
+        public Button initialSelectedButton = null;
+
         protected CanvasController _canvasController;
+        protected List<AButtonController> _buttonControllers;
 
         void Awake()
         {
             _canvasController = GetComponentInParent<CanvasController>();
+            _buttonControllers = GetComponentsInChildren<AButtonController>().ToList();
         }
 
-        public virtual void Enable() => gameObject.Enable();
+        public virtual void Enable()
+        {
+            // Select the first button, if there are any            
+            if (initialSelectedButton == null)
+                _buttonControllers.FirstOrDefault()?.Select();
+            else
+                initialSelectedButton?.Select();
+
+            gameObject.Enable();
+        }
 
         public virtual void Disable() => gameObject.Disable();
     }
